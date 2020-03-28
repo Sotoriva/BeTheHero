2761a02ea69351df0ed0117e67982abb01c708d0
Sotoriva <joaosotoriva@hotmail.com>
2020-03-27 23:06:22 -0300

    Alterações Profile

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,8 +6,6 @@ module.exports = {
 
         const [count] = await connection('incidents').count();
 
-        console.log(count);
-
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
@@ -29,7 +27,7 @@ module.exports = {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
 
-        const [id] = await connection("incidents").insert({
+        const [id] = await connection('incidents').insert({
             title,
             description,
             value,
@@ -48,12 +46,12 @@ module.exports = {
             .select('ong_id')
             .first();
         
-        if (incident.ong_id != ong_id) {
+        if (incident.ong_id !== ong_id) {
             return response.status(401).json({ error: 'Operation not permitted.' });
         }
 
         await connection('incidents').where('id', id).delete();
 
-        return response.select(204).send();
+        return response.status(204).send();
     },
 };
